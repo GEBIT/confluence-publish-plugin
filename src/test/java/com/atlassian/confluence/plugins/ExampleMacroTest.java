250ac66d321b195a42fc1fd443c32ec07e17b018
Matthew Jensen <mjensen@atlassian.com>
2012-09-05 17:41:13 +1000

    Initial Commit

---
+++ src/test/java/com/atlassian/confluence/plugins/ExampleMacroTest.java
@@ -0,0 +1,15 @@
+package com.atlassian.confluence.plugins;
+
+import org.junit.Test;
+
+/**
+ * Testing {@link com.atlassian.confluence.plugins.ExampleMacro}
+ */
+public class ExampleMacroTest
+{
+    @Test
+    public void basic()
+    {
+        // add test here...
+    }
+}
