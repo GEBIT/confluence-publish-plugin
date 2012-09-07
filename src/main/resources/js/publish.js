5e168bcf28a8d5ef1ca993db3a6de1f5ae7939ed
Matthew Jensen <mjensen@atlassian.com>
2012-09-07 13:34:12 +1000

    * Forward to blog post * Spinner step in dialog

--- src/main/resources/js/publish.js
+++ src/main/resources/js/publish.js
@@ -1,36 +1,57 @@
 AJS.toInit(function(){
 
-    var dialog;
+    var popup;
 
     function createDialog() {
         var contextPath = AJS.Confluence.getContextPath();
         var pageId = AJS.params.pageId;
 
-        dialog = new AJS.Dialog(480, 330);
-        dialog.addHeader(AJS.I18n.getText("publish.dialog.asblog.header"), "");
-        dialog.addPanel(AJS.I18n.getText("publish.dialog.asblog.title"), "", "", 1);
-        var template = Confluence.Templates.Publish.publishAsBlog();
-        dialog.getCurrentPanel().html(template);
-        dialog.addCancel(AJS.I18n.getText("cancel.name"), function() {
-            dialog.hide();
+        popup = new AJS.Dialog(480, 330);
+
+        popup.addPage("form-page");
+        popup.addPage("spinner-page");
+        popup.addPage("loading-page");
+
+        popup.gotoPage(1);
+        popup.addPanel(AJS.I18n.getText("publish.dialog.asblog.title"));
+        popup.addHeader(AJS.I18n.getText("publish.dialog.asblog.header"));
+        popup.getCurrentPanel().html(Confluence.Templates.Publish.publishAsBlog());
+        popup.addCancel(AJS.I18n.getText("cancel.name"), function() {
+            popup.hide();
         });
-        dialog.addSubmit(AJS.I18n.getText("publish.dialog.asblog.button"), function() {
+        popup.addSubmit(AJS.I18n.getText("publish.dialog.asblog.button"), function() {
+            popup.gotoPage(2);
             AJS.$.ajax({
                 type:"PUT",
                 url: contextPath + "/rest/publish/1/publish/"+pageId,
-                success: function() {
-                    // TODO: redirect to blog
-                    dialog.hide();
+                success: function(data, status, jqXHR) {
+                    popup.gotoPage(3);
+                    setTimeout(function(){
+                        window.location = jqXHR.getResponseHeader("location");
+                        popup.hide();
+                    }, 1000);
                 }
             })
         });
+
+        popup.gotoPage(2);
+        popup.addPanel("Spinner","#spinner");
+        popup.addHeader(AJS.I18n.getText("publish.dialog.spinner"));
+        popup.getCurrentPanel().html(Confluence.Templates.Publish.spinner());
+
+        popup.gotoPage(3);
+        popup.addPanel("Loading","#loading");
+        popup.addHeader(AJS.I18n.getText("publish.dialog.loading"));
+        popup.getCurrentPanel().html(Confluence.Templates.Publish.loading());
+
+        popup.gotoPage(1);
     }
 
     AJS.$("#publish_asblog").click(function(e) {
-        if(typeof dialog == "undefined") {
+        if(typeof popup == "undefined") {
             createDialog()
         }
-        dialog.show();
+        popup.show();
     });
 
 });
